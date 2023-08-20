9a65e5f2161e4e9ed9e9fe6fef537fca78abc9aa
Justin Tomich <tomichj@gmail.com>
2023-08-20 00:02:35 -0700

    MovieCredit and TvShowCredit now encapsulate a Movie or TVSHow

--- Sources/TMDb/Models/MovieCredit.swift
+++ Sources/TMDb/Models/MovieCredit.swift
@@ -1,118 +1,40 @@
 import Foundation
 
-///
-/// A model representing a movie.
-///
 public struct MovieCredit: Identifiable, Codable, Equatable, Hashable {
 
-    ///
-    /// Movie identifier.
-    ///
-    public let id: Int
+    public let movie: Movie
     public let creditID: String
-
-    ///
-    /// Movie title.
-    ///
-    public let title: String
-
-    ///
-    /// Original movie title.
-    ///
-    public let originalTitle: String?
-
-    ///
-    /// Original language of the movie.
-    ///
-    public let originalLanguage: String?
-
-    ///
-    /// Movie overview.
-    ///
-    public let overview: String?
-
-    ///
-    /// Movie genres.
-    ///
-    public let genres: [Genre]?
-
-    ///
-    /// Movie release date.
-    ///
-    public let releaseDate: Date?
-
-    ///
-    /// Movie poster path.
-    ///
-    /// To generate a full URL see <doc:/TMDb/GeneratingImageURLs>.
-    ///
-    public let posterPath: URL?
-
-    ///
-    /// Movie poster backdrop path.
-    ///
-    /// To generate a full URL see <doc:/TMDb/GeneratingImageURLs>.
-    ///
-    public let backdropPath: URL?
-
-    ///
-    /// Current popularity.
-    ///
-    public let popularity: Double?
-
-    ///
-    /// Average vote score.
-    ///
-    public let voteAverage: Double?
-
-    ///
-    /// Number of votes.
-    ///
-    public let voteCount: Int?
-
-    ///
-    /// Has video.
-    ///
-    public let hasVideo: Bool?
-
-    ///
-    /// Is the movie only suitable for adults.
-    ///
-    public let isAdultOnly: Bool?
-
     public let department: String?
     public let job: String?
     public let character: String?
 
+    public var id: Int { return movie.id }
+    var popularity: Double? { return movie.popularity }
+    var releaseDate: Date? { return movie.releaseDate }
     
-    
-    ///
-    /// Creates a movie object.
-    ///
-    /// - Parameters:
-    ///    - id: Movie identifier.
-    ///    - title: Movie title.
-    ///    - tagline: Movie tagline.
-    ///    - originalTitle: Original movie title.
-    ///    - originalLanguage: Original language of the movie.
-    ///    - overview: Movie overview.
-    ///    - genres: Movie genres.
-    ///    - releaseDate: Movie release date.
-    ///    - posterPath: Movie poster path.
-    ///    - backdropPath: Movie poster backdrop path.
-    ///    - popularity: Current popularity.
-    ///    - voteAverage: Average vote score.
-    ///    - voteCount: Number of votes.
-    ///    - hasVideo: Has video.
-    ///    - isAdultOnly: Is the movie only suitable for adults.
-    ///
+    public init(
+        movie: Movie,
+        creditID: String,
+        department: String? = nil,
+        job: String? = nil,
+        character: String? = nil
+    ) {
+        self.movie = movie
+        self.creditID = creditID
+        self.department = department
+        self.job = job
+        self.character = character
+    }
+
     public init(
         id: Int,
         creditID: String,
         title: String,
+        tagline: String? = nil,
         originalTitle: String? = nil,
         originalLanguage: String? = nil,
         overview: String? = nil,
+        runtime: Int? = nil,
         genres: [Genre]? = nil,
         releaseDate: Date? = nil,
         posterPath: URL? = nil,
@@ -126,46 +48,35 @@ public struct MovieCredit: Identifiable, Codable, Equatable, Hashable {
         job: String? = nil,
         character: String? = nil
     ) {
-        self.id = id
+        self.movie = .init(
+            id: id,
+            title: title,
+            tagline: tagline,
+            originalTitle: originalTitle,
+            originalLanguage: originalLanguage,
+            overview: overview,
+            runtime: runtime,
+            genres: genres,
+            releaseDate: releaseDate,
+            posterPath: posterPath,
+            backdropPath: backdropPath,
+            popularity: popularity,
+            voteAverage: voteAverage,
+            voteCount: voteCount,
+            hasVideo: hasVideo,
+            isAdultOnly: isAdultOnly
+        )
         self.creditID = creditID
-        self.title = title
-        self.originalTitle = originalTitle
-        self.originalLanguage = originalLanguage
-        self.overview = overview
-        self.genres = genres
-        self.releaseDate = releaseDate
-        self.posterPath = posterPath
-        self.backdropPath = backdropPath
-        self.popularity = popularity
-        self.voteAverage = voteAverage
-        self.voteCount = voteCount
-        self.hasVideo = hasVideo
-        self.isAdultOnly = isAdultOnly
         self.department = department
         self.job = job
         self.character = character
     }
-
 }
 
 extension MovieCredit {
 
     private enum CodingKeys: String, CodingKey {
-        case id
         case creditID = "creditId"
-        case title
-        case originalTitle
-        case originalLanguage
-        case overview
-        case genres
-        case releaseDate
-        case posterPath
-        case backdropPath
-        case popularity
-        case voteAverage
-        case voteCount
-        case hasVideo = "video"
-        case isAdultOnly = "adult"
         case department
         case job
         case character
@@ -173,33 +84,9 @@ extension MovieCredit {
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        let container2 = try decoder.container(keyedBy: CodingKeys.self)
-
-        self.id = try container.decode(Int.self, forKey: .id)
+        
+        self.movie = try Movie(from: decoder)
         self.creditID = try container.decode(String.self, forKey: .creditID)
-        self.title = try container.decode(String.self, forKey: .title)
-        self.originalTitle = try container.decodeIfPresent(String.self, forKey: .originalTitle)
-        self.originalLanguage = try container.decodeIfPresent(String.self, forKey: .originalLanguage)
-        self.overview = try container.decodeIfPresent(String.self, forKey: .overview)
-        self.genres = try container.decodeIfPresent([Genre].self, forKey: .genres)
-
-        // Need to deal with empty strings - date decoding will fail with an empty string
-        let releaseDateString = try container.decodeIfPresent(String.self, forKey: .releaseDate)
-        self.releaseDate = try {
-            guard let releaseDateString, !releaseDateString.isEmpty else {
-                return nil
-            }
-
-            return try container2.decodeIfPresent(Date.self, forKey: .releaseDate)
-        }()
-
-        self.posterPath = try container.decodeIfPresent(URL.self, forKey: .posterPath)
-        self.backdropPath = try container.decodeIfPresent(URL.self, forKey: .backdropPath)
-        self.popularity = try container.decodeIfPresent(Double.self, forKey: .popularity)
-        self.voteAverage = try container.decodeIfPresent(Double.self, forKey: .voteAverage)
-        self.voteCount = try container.decodeIfPresent(Int.self, forKey: .voteCount)
-        self.hasVideo = try container.decodeIfPresent(Bool.self, forKey: .hasVideo)
-        self.isAdultOnly = try container.decodeIfPresent(Bool.self, forKey: .isAdultOnly)
         self.department = try container.decodeIfPresent(String.self, forKey: .department)
         self.job = try container.decodeIfPresent(String.self, forKey: .job)
         self.character = try container.decodeIfPresent(String.self, forKey: .character)
--- Sources/TMDb/Models/TvShowCredit.swift
+++ Sources/TMDb/Models/TvShowCredit.swift
@@ -5,117 +5,33 @@ import Foundation
 ///
 public struct TVShowCredit: Identifiable, Codable, Equatable, Hashable {
 
-    ///
-    /// TV show identifier.
-    ///
-    public let id: Int
-
+    public let tvShow: TVShow
     public let creditID: String
-
-    ///
-    /// TV show name.
-    ///
-    public let name: String
-
-    ///
-    /// Original TV show name.
-    ///
-    public let originalName: String?
-
-    ///
-    /// Original language of the TV show.
-    ///
-    public let originalLanguage: String?
-
-    ///
-    /// TV show overview.
-    ///
-    public let overview: String?
-
-    ///
-    /// TV show genres.
-    ///
-    public let genres: [Genre]?
-
-    ///
-    /// TV show's first air date.
-    ///
-    public let firstAirDate: Date?
-
-    ///
-    /// TV show country of origin.
-    ///
-    public let originCountry: [String]?
-
-    ///
-    /// TV show poster path.
-    ///
-    /// To generate a full URL see <doc:/TMDb/GeneratingImageURLs>.
-    ///
-    public let posterPath: URL?
-
-    ///
-    /// TV show backdrop path.
-    ///
-    /// To generate a full URL see <doc:/TMDb/GeneratingImageURLs>.
-    ///
-    public let backdropPath: URL?
-
-    ///
-    /// TV show current popularity.
-    ///
-    public let popularity: Double?
-
-    ///
-    /// Average vote score.
-    ///
-    public let voteAverage: Double?
-
-    ///
-    /// Number of votes.
-    ///
-    public let voteCount: Int?
-
-    ///
-    /// Is the TV show only suitable for adults.
-    ///
-    public let isAdultOnly: Bool?
-    
     public let department: String?
     public let job: String?
     public let character: String?
     public let episodeCount: Int?
+    
+    public var id: Int { return tvShow.id }
+    var popularity: Double? { return tvShow.popularity }
+    var firstAirDate: Date? { return tvShow.firstAirDate }
 
-    ///
-    /// Creates a TV show object.
-    ///
-    /// - Parameters:
-    ///    - id: TV show identifier.
-    ///    - name: TV show name.
-    ///    - originalName: Original TV show name.
-    ///    - originalLanguage: Original language of the TV show.
-    ///    - overview: TV show overview.
-    ///    - episodeRunTime: TV show episode run times, in minutes.
-    ///    - numberOfSeasons: Number of seasons in the TV show.
-    ///    - numberOfEpisodes: Total number of episodes in the TV show.
-    ///    - seasons: Seasons in the TV show.
-    ///    - genres: TV show genres.
-    ///    - firstAirDate: TV show's first air date.
-    ///    - originCountry: TV show country of origin.
-    ///    - posterPath: TV show poster path.
-    ///    - backdropPath: TV show backdrop path.
-    ///    - homepageURL: TV show's web site URL.
-    ///    - isInProduction: Is TV show currently in production.
-    ///    - languages: Languages the TV show is available in.
-    ///    - lastAirDate: Last air date of the TV show.
-    ///    - networks: Networks involved in the TV show.
-    ///    - productionCompanies: Production companies involved in the TV show.
-    ///    - status: TV show status.
-    ///    - popularity: TV show current popularity.
-    ///    - voteAverage: Average vote score.
-    ///    - voteCount: Number of votes.
-    ///    - isAdultOnly: Is the TV show only suitable for adults.
-    ///
+    public init(
+        tvShow: TVShow,
+        creditID: String,
+        department: String? = nil,
+        job: String? = nil,
+        character: String? = nil,
+        episodeCount: Int? = nil
+    ) {
+        self.tvShow = tvShow
+        self.creditID = creditID
+        self.department = department
+        self.job = job
+        self.character = character
+        self.episodeCount = episodeCount
+    }
+    
     public init(
         id: Int,
         creditID: String,
@@ -137,47 +53,37 @@ public struct TVShowCredit: Identifiable, Codable, Equatable, Hashable {
         character: String? = nil,
         episodeCount: Int? = nil
     ) {
-        self.id = id
+        self.tvShow = .init(
+            id: id,
+            name: name,
+            originalName: originalName,
+            originalLanguage: originalLanguage,
+            overview: overview,
+            genres: genres,
+            firstAirDate: firstAirDate,
+            originCountry: originCountry,
+            posterPath: posterPath,
+            backdropPath: backdropPath,
+            popularity: popularity,
+            voteAverage: voteAverage,
+            voteCount: voteCount,
+            isAdultOnly: isAdultOnly
+        )
         self.creditID = creditID
-        self.name = name
-        self.originalName = originalName
-        self.originalLanguage = originalLanguage
-        self.overview = overview
-        self.genres = genres
-        self.firstAirDate = firstAirDate
-        self.originCountry = originCountry
-        self.posterPath = posterPath
-        self.backdropPath = backdropPath
-        self.popularity = popularity
-        self.voteAverage = voteAverage
-        self.voteCount = voteCount
-        self.isAdultOnly = isAdultOnly
         self.department = department
         self.job = job
         self.character = character
         self.episodeCount = episodeCount
     }
+    
+
 
 }
 
 extension TVShowCredit {
 
     private enum CodingKeys: String, CodingKey {
-        case id
         case creditID = "creditId"
-        case name
-        case originalName
-        case originalLanguage
-        case overview
-        case genres
-        case firstAirDate
-        case originCountry
-        case posterPath
-        case backdropPath
-        case popularity
-        case voteAverage
-        case voteCount
-        case isAdultOnly = "adult"
         case department
         case job
         case character
@@ -186,33 +92,9 @@ extension TVShowCredit {
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        let container2 = try decoder.container(keyedBy: CodingKeys.self)
-
-        self.id = try container.decode(Int.self, forKey: .id)
+        
+        self.tvShow = try TVShow(from: decoder)
         self.creditID = try container.decode(String.self, forKey: .creditID)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.originalName = try container.decodeIfPresent(String.self, forKey: .originalName)
-        self.originalLanguage = try container.decodeIfPresent(String.self, forKey: .originalLanguage)
-        self.overview = try container.decodeIfPresent(String.self, forKey: .overview)
-        self.genres = try container.decodeIfPresent([Genre].self, forKey: .genres)
-
-        // Need to deal with empty strings - date decoding will fail with an empty string
-        let firstAirDateString = try container.decodeIfPresent(String.self, forKey: .firstAirDate)
-        self.firstAirDate = try {
-            guard let firstAirDateString, !firstAirDateString.isEmpty else {
-                return nil
-            }
-
-            return try container2.decodeIfPresent(Date.self, forKey: .firstAirDate)
-        }()
-
-        self.originCountry = try container.decodeIfPresent([String].self, forKey: .originCountry)
-        self.posterPath = try container.decodeIfPresent(URL.self, forKey: .posterPath)
-        self.backdropPath = try container.decodeIfPresent(URL.self, forKey: .backdropPath)
-        self.popularity = try container.decodeIfPresent(Double.self, forKey: .popularity)
-        self.voteAverage = try container.decodeIfPresent(Double.self, forKey: .voteAverage)
-        self.voteCount = try container.decodeIfPresent(Int.self, forKey: .voteCount)
-        self.isAdultOnly = try container.decodeIfPresent(Bool.self, forKey: .isAdultOnly)
         self.department = try container.decodeIfPresent(String.self, forKey: .department)
         self.job = try container.decodeIfPresent(String.self, forKey: .job)
         self.character = try container.decodeIfPresent(String.self, forKey: .character)
