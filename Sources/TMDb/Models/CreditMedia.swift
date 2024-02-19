a7d3b89f2bdeaa79a030f4bff73c180e1ffa36e8
Justin Tomich <tomichj@gmail.com>
2024-02-18 19:18:33 -0800

    CreditMedia - added most missing fields

--- Sources/TMDb/Models/CreditMedia.swift
+++ Sources/TMDb/Models/CreditMedia.swift
@@ -2,8 +2,18 @@ import Foundation
 
 public struct CreditMedia: Identifiable, Codable, Equatable, Hashable {
     public let id: Int
+    public let adult: Bool?
     public let backdropPath: URL?
     public let character: String?
+    public let name: String?
+    public let originalName: String?
+    public let overview: String
+    public let posterPath: URL?
+//    public let firstAirDate: Date?
+    public let firstAirDate: String?
+    public let popularity: Double?
+    public let voteAverage: Double?
+    public let voteCount: Int?
     public let episodes: [TVEpisode]?
     public let seasons: [TVSeason]?
 }
