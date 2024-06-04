ff848352a286b902b849b07c8f56f2c1183c12d4
Justin Tomich <tomichj@gmail.com>
2024-06-03 19:47:46 -0700

    alternateTitle renamed iso_3166_1 -> countryCode

--- Sources/TMDb/Models/AlternateTitle.swift
+++ Sources/TMDb/Models/AlternateTitle.swift
@@ -1,7 +1,17 @@
 import Foundation
 
 public struct AlternativeTitle: Codable, Equatable, Hashable {
-    public let iso_3166_1: String?
+    public let countryCode: String?
     public let title: String?
     public let type: String?
+}
+
+extension AlternativeTitle {
+    
+    private enum CodingKeys: String, CodingKey {
+        case countryCode = "iso31661"
+        case title
+        case type
+    }
+    
 }
